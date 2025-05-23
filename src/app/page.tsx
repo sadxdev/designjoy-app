e3ce15311cd8dd13ffdad7d475a01bfe0f58afbe
sadxdev <sadxdev@gmail.com>
2025-05-23 10:50:42 +0530

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 export default function Home() {
   return (
     <div>
-      <div>Hi, developers!</div>
       <div className="bg-green-500 text-white text-2xl p-6 rounded-s-2xl">
         ✅ Tailwind v4 is working!
       </div>
