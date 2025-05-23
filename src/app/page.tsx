8b116ea2ae10126501d7835f4047ee8ad0c5de28
sadxdev <sadxdev@gmail.com>
2025-05-23 10:42:34 +0530

    installed framer motion, heroku, icons

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>Hi, developers!</div>
+      <div className="bg-green-500 text-white text-2xl p-6 rounded-s-2xl">
+        ✅ Tailwind v4 is working!
+      </div>
+    </div>
+  );
+}
